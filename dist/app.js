7b78f8520c96eb7de73e8ce8020d24208459fa9c
gregoryb79 <gregoryb79@gmail.com>
2025-01-22 21:12:19 +0200

    trim

--- dist/app.js
+++ dist/app.js
@@ -67,13 +67,13 @@ function workersRecordsystem() {
     while (userInput !== null) {
         switch (userInput) {
             case "1":
-                gradesRecord = addWorker(employeesList);
+                employeesList = addWorker(employeesList);
                 break;
             case "2":
                 alert(printWorkers(employeesList));
                 break;
             case "3":
-                gradesRecord = removeWorker(employeesList);
+                employeesList = removeWorker(employeesList);
                 break;
             case "4":
                 getStatistics(employeesList);
